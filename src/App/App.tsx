70f2babc5fc43600b276e827ad56ac997af38343
manu <manu.8640@hotmail.fr>
2022-05-30 15:38:46 +0100

    props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Button from './components/Button/Bouton'
+import Button from "./components/Button/Bouton";
+//import ButtonStyle from "./components.Button.module.css"
 import './App.css';
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
 
      <Button > <div>TOTO</div><div>titi</div> </Button> 
 
-     <Button> hello<hr/>de lu </Button>
+     <Button style={{fontWeigth:900,fontSize:'x-large'}}> hello<hr/>de lu </Button>
 
      <Button bgColor='tomato' color="black">hello<hr/>de lu </Button>
 
