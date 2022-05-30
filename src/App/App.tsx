358d538c1f6f38f5b84e38e7090eb70f312a4642
manu <manu.8640@hotmail.fr>
2022-05-30 11:05:07 +0100

    reformulaton+ vidange

---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { json } from 'stream/consumers';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // comment js
+  /* coment multiligne */
+  return (
+    <div className="App">
+      {/* commentaire  */}
+     DEMAT BREIZH
+    </div>
+  );
+}
+
+export default App;
