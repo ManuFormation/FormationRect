6abf8b6a45bdd954945d3e84ddc5864b5c2f623e
manu <manu.8640@hotmail.fr>
2022-05-31 09:22:38 +0100

    ajout des actions

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, {} from 'react';
+import React from 'react';
 import Button from './components/Button/Bouton';
 interface IAppState{
 //message:'hello'|'good bye'|undefined , message present mais peut être vide
@@ -13,20 +13,29 @@ class App extends React.Component<IAppProps,IAppState> {
     super(props);
     this.state={message:'hello', counter:0}    
   }
+  componentDidMount(){
+    console.log('Composant app monté')
+  }
+  componentDidUpdate(prevProps:IAppProps, prevState:IAppState){
+    console.log('State post update =>', prevState, this.state);
+    console.log('Props post update =>', prevProps, this.props);
+
+  }
 
   render() {
     return <div className='App' style={{textAlign:'center'}}>
         Valeur compteur : {this.state.counter}
         <hr/>
       <Button action={()=>{
-        this.setState({counter:this.state.counter-1})      
+        this.setState({counter:this.state.counter-1})  
+        console.log(this.state.counter)    
       }} bgColor='tomato'> decrement-1</Button>
       
       <Button action={()=>{
-        this.setState({counter:this.state.counter+1})     
+        this.setState({counter:this.state.counter+1})  
+        console.log(this.state.counter)   
       }} bgColor='skyblue'>increment+1</Button>
-    </div>
-    
+    </div>    
   }
 }
 
