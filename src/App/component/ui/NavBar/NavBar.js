31d3b9376aef9787e701e1b83314e4c41cb25a75
manu <manu.8640@hotmail.fr>
2022-06-01 16:34:36 +0100

    LinkContainer react-router-bootstrap

--- src/App/component/ui/NavBar/NavBar.js
+++ src/App/component/ui/NavBar/NavBar.js
@@ -1,25 +1,34 @@
-import React,{ useEffect,useState} from 'react';
-import PropTypes from 'prop-types';
-import styles from './Navbar.module.css';
-import {Container,Nav, Navbar  } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavBar = (props) => (
   <div className={styles.Navbar} data-testid="Navbar">
-  
-  <Navbar bg="primary" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>   
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>Navbar</Navbar.Brand>
+        </LinkContainer>
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/editor">
+            <Nav.Link>new Meme</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/thumbail">
+            <Nav.Link>Thumbail</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/editor/0">
+            <Nav.Link>editv id 0</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Container>
+    </Navbar>
   </div>
 );
-
-
 
 export default NavBar;
